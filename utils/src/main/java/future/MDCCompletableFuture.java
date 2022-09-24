1ec593ff629cc19c30bfafcbb85780f629cb4397
alimojahed <alimojahed0079@gmail.com>
2022-09-24 08:29:47 +0330

    [FIX] - clear completable future after task is done

--- utils/src/main/java/future/MDCCompletableFuture.java
+++ utils/src/main/java/future/MDCCompletableFuture.java
@@ -4,8 +4,6 @@ import org.slf4j.MDC;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ForkJoinTask;
 import java.util.function.Supplier;
 
 /**
@@ -16,16 +14,26 @@ public class MDCCompletableFuture<T> extends CompletableFuture<T> {
     public static <U> CompletableFuture<U> supplyAsync(Supplier<U> supplier) {
         Map<String, String> mdc = MDC.getCopyOfContextMap();
         return CompletableFuture.supplyAsync(() -> {
-            MDC.setContextMap(mdc);
-            return supplier.get();
+            try {
+                MDC.setContextMap(mdc);
+                return supplier.get();
+
+            } finally {
+                MDC.clear();
+            }
         });
     }
 
     public static CompletableFuture<Void> runAsync(Runnable command) {
         Map<String, String> mdc = MDC.getCopyOfContextMap();
         return CompletableFuture.runAsync(() -> {
-            MDC.setContextMap(mdc);
-             command.run();
+            try {
+                MDC.setContextMap(mdc);
+                command.run();
+
+            } finally {
+                MDC.clear();
+            }
         });
     }
 
